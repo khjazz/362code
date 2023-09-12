d020eb7e9beb7e16095c0916db0ccd89eac9308b
khjazz <142329574+khjazz@users.noreply.github.com>
2023-09-12 18:47:59 +0800

    Add files via upload

---
+++ lab01.py
@@ -0,0 +1,16 @@
+# File: lab01.py
+
+# Replace the following by your full name and 8-digit student number
+student_name = "Chan Tai Man"
+student_id = "01234567"
+
+from random import random
+
+def palindrome_recur(s):
+    pass  # replace this line by your code
+
+def myth_value(n):
+    pass  # replace this line by your code
+
+if __name__ == "__main__":
+    pass  # replace this line by your code (if necessary)
