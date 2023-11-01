b5628c62219ad1c525b984ded4288444ad252b46
khjazz <142329574+khjazz@users.noreply.github.com>
2023-11-01 03:30:22 +0000

    lab08a

--- t08-src/lab08a.py
+++ t08-src/lab08a.py
@@ -1,7 +1,28 @@
 # File: lab08a.py
 
 # Replace the following by your full name and 8-digit student number
-student_name = "Chan Tai Man"
-student_id = "01234567"
+student_name = "Tsang Ka Ho"
+student_id = "12621133"
 
 # Write code below
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/<int(signed=True):a>/plus/<int(signed=True):b>")
+def add(a, b):
+    return f"{a} + {b} = {a+b}"
+
+@app.route("/<int(signed=True):a>/minus/<int(signed=True):b>")
+def subtract(a, b):
+    return f"{a} - {b} = {a-b}"
+
+@app.route("/<int(signed=True):a>/times/<int(signed=True):b>")
+def multiply(a, b):
+    return f"{a} * {b} = {a*b}"
+
+@app.route("/<int(signed=True):a>/over/<int(signed=True):b>")
+def divide(a, b):
+    return f"{a} / {b} = {a/b}"
+
+if __name__ == "__main__":
+    app.run()
