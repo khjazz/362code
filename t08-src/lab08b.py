9a4a3b18a485b4a503d1e5032b6d2d79045a6e89
khjazz <142329574+khjazz@users.noreply.github.com>
2023-11-01 05:16:35 +0000

    lab08b

--- t08-src/lab08b.py
+++ t08-src/lab08b.py
@@ -1,7 +1,56 @@
 # File: lab08b.py
 
 # Replace the following by your full name and 8-digit student number
-student_name = "Chan Tai Man"
-student_id = "01234567"
+student_name = "Tsang Ka Ho"
+student_id = "12621133"
 
 # Write code below
+from flask import (Flask, redirect, render_template_string,
+                   request, url_for)
+app = Flask(__name__)
+
+template_str = """
+<h2>Account</h2>
+Balance: {{balance}}<br />
+Last transaction: {{last_txn}}<br />
+<br />
+<form action="/deposit" method="POST">
+<input type="text" name="amount" />
+<input type="submit" value="Deposit" />
+</form>
+<form action="/withdraw" method="POST">
+<input type="text" name="amount" />
+<input type="submit" value="Withdraw" />
+</form>
+"""
+
+balance = 0
+last_txn = ""
+
+@app.route("/")
+def account_display():
+    # todos = [(id, desc, url_for("todo_delete", id=id))
+    #         for id, desc in enumerate(todo)]
+    return render_template_string(template_str, balance=balance, last_txn=last_txn)
+
+@app.route("/deposit", methods=["POST"])
+def Deposit():
+    amount = int(request.form["amount"].strip())
+    global balance, last_txn
+    balance = balance + amount
+    last_txn = f"deposit {amount}"
+    return redirect("/")
+
+@app.route("/withdraw", methods=["POST"])
+def Withdraw():
+    global balance, last_txn
+    amount = int(request.form["amount"].strip())
+    if balance >= amount:
+        balance = balance - amount
+        last_txn = f"withdraw {amount}"
+    else:
+        last_txn = f"withdraw {amount} (failed)"
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run()
