21e023292c51590c57dcb7aba6fb4e91277d35a9
khjazz <homeworkccc@gmail.com>
2023-11-23 20:44:41 +0800

    Add lab11a and lab11b

--- t11-src/lab11a.py
+++ t11-src/lab11a.py
@@ -1,8 +1,8 @@
 # File: lab11a.py
 
 # Replace the following by your full name and 8-digit student number
-student_name = "Chan Tai Man"
-student_id = "01234567"
+student_name = "Tsang Ka Ho"
+student_id = "12621133"
 
 # Modify code below
 from multiprocessing import Pipe, Process
@@ -10,7 +10,51 @@ from multiprocessing import Pipe, Process
 SENTINEL = -1
 
 def not_multiple(conn_in, conn_out, num):
-    pass
+    while True:
+        obj = conn_in.recv()
+        if obj == SENTINEL:
+            conn_out.send(SENTINEL)
+            break
+        if not obj % num == 0 or obj == num:
+            conn_out.send(obj)
 
 if __name__ == "__main__":
-    pass
+    conn1_in, conn1_out = Pipe(False)
+    conn2_in, conn2_out = Pipe(False)
+    conn3_in, conn3_out = Pipe(False)
+    conn4_in, conn4_out = Pipe(False)
+    conn5_in, conn5_out = Pipe(False)
+
+    Process(target=not_multiple, args=[conn1_in, conn2_out, 2]).start()
+    Process(target=not_multiple, args=[conn2_in, conn3_out, 3]).start()
+    Process(target=not_multiple, args=[conn3_in, conn4_out, 5]).start()
+    Process(target=not_multiple, args=[conn4_in, conn5_out, 7]).start()
+
+    for i in range(2, 100):
+        conn1_out.send(i)
+    conn1_out.send(SENTINEL)
+    while True:
+        obj = conn5_in.recv()
+        if obj == SENTINEL:
+            break
+        print(obj, end=" ", flush=True)
+
+"""
+With reference to the above pipe_unidir.py program, write a program that contains a
+not_multiple() function and has 4 child processes and 5 pipes.
+The function not_multiple(conn_in, conn_out, num) passes from conn_in to conn_out the
+integer num and integers that are not multiples of num. For example, when num is 3, the function
+passes 1, 2, 3, 4, 5, 7, 8, 10, 11, 13, etc (i.e. it does not pass 6, 9, 12, etc).
+The main process, 4 child processes, and 5 unidirectional pipes work as follows:
+• The main process sends integers from 2 to 99 (both inclusive) to the first child process.
+• The first child process removes multiples of 2 (except 2 itself) and sends the remaining integers
+to the second child process.
+• The second child process removes multiples of 3 (except 3 itself) and sends the rest to the third
+child process.
+• The third and fourth child processes do similar tasks by removing multiples of 5 and 7, respectively, in the integers they receive.
+• The main process receives integers from the fourth child process and print them out.
+The results are the prime numbers between 2 and 99!
+2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
+Write the program in the lab11a.py file for submission. Remember to enforce good programming
+style in your code
+"""
--- t11-src/lab11b.py
+++ t11-src/lab11b.py
@@ -1,7 +1,70 @@
 # File: lab11b.py
 
 # Replace the following by your full name and 8-digit student number
-student_name = "Chan Tai Man"
-student_id = "01234567"
+student_name = "Tsang Ka Ho"
+student_id = "12621133"
 
 # Write code below
+import time
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+
+class Account():
+    _DEPOSIT = 'deposit'
+    _WITHDRAW = 'withdraw'
+
+    def __init__(self):
+        self._balance = 0
+
+    def get_balance(self):
+        return self._balance
+
+    def _deposit(self, amount):
+        self._balance += amount
+
+    def _withdraw(self, amount):
+        self._balance -= amount
+
+    def handle_request(self, obj, socket):
+        op = obj["operation"]
+        amount = obj["amount"]
+        if op == self._DEPOSIT:
+            self._deposit(amount)
+            socket.send_json({"status": "success", "balance": self.get_balance()})
+        elif op == self._WITHDRAW:
+            if self.get_balance() >= amount:
+                self._withdraw(amount)
+                socket.send_json({"status": "success", "balance": self.get_balance()})
+            else:
+                socket.send_json({"status": "failure (insufficient funds)", "balance": self.get_balance()})
+
+account = Account()
+while True:
+    obj = socket.recv_json()
+    account.handle_request(obj, socket)
+
+"""
+Write a ZeroMQ request-reply server to perform deposit and withdrawal operations on an account
+using JSON messages. The account balance, deposit amounts and withdrawal amounts are integers.
+A withdrawal is only carried out when the existing balance is sufficient, i.e. when balance >= amount.
+In other words, the balance must be positive or zero at any time. The operations are described as
+follows.
+The request data for a deposit operation has the JSON format: {"operation": "deposit",
+"amount": x }, where x is a positive integer or zero. Upon receiving such a request, the server
+updates the balance and returns the JSON message { "status": "success", "balance": x },
+where x is the current balance.
+The request data for a withdrawal operation has the JSON format: {"operation": "withdraw",
+"amount": x }, where x is a positive integer or zero. Upon receiving such a request, the server does
+the following:
+• If the balance is sufficient for the withdrawal amount, the server updates the balance and
+returns the JSON message { "status": "success", "balance": x }, where x is the current balance.
+• If the balance is insufficient for the withdrawal amount, the server returns the JSON message
+{ "status": "failure (insufficient funds)", "balance": x }, where x is the current balance.
+Use the TCP port 5555. No validation of the request data is required.
+Write the program in the lab11b.py file for submission. Remember to enforce good programming
+style in your code.
+"""
