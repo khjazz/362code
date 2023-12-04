beb8276bf17aded6aa295b3ac8227f144af5d16a
khjazz <homeworkccc@gmail.com>
2023-12-04 10:59:31 +0800

    Add protocol validation in get_quotes function

--- project/src/quote_client.py
+++ project/src/quote_client.py
@@ -8,6 +8,8 @@ def get_quotes(host, port, protocol, concurrency, executor=ThreadPoolExecutor())
     Use a thread pool to start multiple clients to get quotes from the quote server.
     Protocol can be "tcp" or "udp".
     """
+    if protocol not in ["tcp", "udp"]:
+        raise ValueError("protocol must be tcp or udp")
     try:
         res = executor.map(
             start_tcp_client if protocol == "tcp" else start_udp_client,
@@ -36,5 +38,5 @@ def start_udp_client(host, port):
     """
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.sendto(b"", (host, port))
-        data, addr = s.recvfrom(1024)
+        data, _ = s.recvfrom(1024)
         return data.decode()
